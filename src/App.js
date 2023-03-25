b3f8dcd66db2eacc017a3aff096da6a68ecf6412
Naman Bagri <namanbagri@Namans-MacBook-Pro.local>
2023-03-25 13:16:29 +0530

    added layout for dashboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { DefaultLayout } from './layout/DefaultLayout';
 import { Entry } from './pages/entry/Entry.page';
 
 function App() {
   return (
     <div className="App">
-      <Entry></Entry>
+      {/* <Entry></Entry> */}
+      <DefaultLayout>
+        SEND PAGE CONTENT
+      </DefaultLayout>
     </div>
   );
 }
